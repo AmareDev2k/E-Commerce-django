630f59c90131b418d70eec2d184fb3be78237751
Aravinda Amarasingha <supunaravinda2004@gmail.com>
2026-08-17 03:03:45 +0530

    feat: implement shopping cart functionality with context state management and UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductList from "./pages/ProductList.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/product/:id" element={<ProductDetails/>} />
+        <Route path="/cart" element={<CartPage/>} />
       </Routes>
     </Router>
   );
